bb2863de26f5b60209dd365c7685833f695f78b6
hyeonggook.lee <hyeonggook.lee@gluwa.com>
2021-05-20 15:07:39 +0900

    Use timestamp for nonce

--- src/Gluwa.php
+++ src/Gluwa.php
@@ -318,7 +318,8 @@ class Gluwa {
         if ($Result['code'] >= 200 && $Result['code'] <= 300) {
             $Fee = $Result['response']['MinimumFee'];
 
-            $Nonce = $this->getCryptoRandom(75);
+            $Nonce = time();
+            // $Nonce = $this->getCryptoRandom(75);
 
             $ConvertedAmount = strval(Ethereum::toWei($Amount, "ether"));
             $ConvertedFee = strval(Ethereum::toWei($Fee, "ether"));
