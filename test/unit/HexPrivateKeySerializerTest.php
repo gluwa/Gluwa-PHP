2ec45f0cc687eeb182f9623d040a4877f715a95a
Will <83000144+will-gluwa@users.noreply.github.com>
2021-12-09 17:24:50 -0800

    Update HexPrivateKeySerializerTest.php

--- test/unit/HexPrivateKeySerializerTest.php
+++ test/unit/HexPrivateKeySerializerTest.php
@@ -11,8 +11,7 @@ class HexPrivateKeySerializerTest extends TestCase
 {
     protected $serializer;
 
-    public function setUp()
-    {
+    protected function setUp(): void {
         parent::setUp();
         $generator = CurveFactory::getGeneratorByName(SecgCurve::NAME_SECP_256K1);
 
