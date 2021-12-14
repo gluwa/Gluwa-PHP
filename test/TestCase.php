59c576ad1971f3d62b8c33cbacb2820c6c88f1a0
Will <83000144+will-gluwa@users.noreply.github.com>
2021-12-14 10:17:01 -0800

    Update TestCase.php

--- test/TestCase.php
+++ test/TestCase.php
@@ -9,15 +9,14 @@ use Gluwa\Secp256k1\Serializer\HexSignatureSerializer;
 class TestCase extends BaseTestCase
 {
     protected $sigSerializer;
-
     protected $signed = '';
-
-    protected $testPrivateKey = getenv("GLUWA_ETHEREUM_PRIVATE_KEY");
-
-    protected $testPublicKey = getenv("GLUWA_ETHEREUM_ADDRESS");
+    protected $testPrivateKey;
+    protected $testPublicKey;
 
     protected function setUp(): void {
         $this->sigSerializer = new HexSignatureSerializer();
+        $this->testPrivateKey = getenv("GLUWA_ETHEREUM_PRIVATE_KEY");
+        $this->testPublicKey = getenv("GLUWA_ETHEREUM_ADDRESS");
         parent::setUp();
     }
 
